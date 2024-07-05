cf5712daf616a66103878eefa6f7d2f5bb1e5562
mpreams <mreams97@gmail.com>
2024-07-05 01:08:03 -0400

    Add numbers to display when clicked

--- script.js
+++ script.js
@@ -18,3 +18,45 @@ const operate = function(first, operator, second) {
     if (operator === '*') return multiply(first, second);
     if (operator === '/') return divide(first, second);
 }
+
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const seven = document.querySelector('#seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+const clear = document.querySelector('#clear');
+const zero = document.querySelector('#zero');
+const equals = document.querySelector('#equals');
+const display = document.querySelector('#display');
+
+one.onclick = () => {
+    display.textContent += 1;
+}
+two.onclick = () => {
+    display.textContent += 2;
+}
+three.onclick = () => {
+    display.textContent += 3;
+}
+four.onclick = () => {
+    display.textContent += 4;
+}
+five.onclick = () => {
+    display.textContent += 5;
+}
+six.onclick = () => {
+    display.textContent += 6;
+}
+seven.onclick = () => {
+    display.textContent += 7;
+}
+eight.onclick = () => {
+    display.textContent += 8;
+}
+nine.onclick = () => {
+    display.textContent += 9;
+}
